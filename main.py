90a52634133068d3f1c5e2921175491d1614b46d
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-01-20 16:13:51 +0800

    test update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import API_Data_Retrieves as api 
 apikey = "YTK6VG8ZMVOB0Z0H"
-alpah_vantage = api.FundamentalData(apikey)
-data = alpah_vantage.GetListingDelistingStatus_Original()
+alpah_vantage1 = api.StockTimeSeries(apikey)
+alpah_vantage2 = api.FundamentalData(apikey)
+#data = alpah_vantage1.GetWeeklyStockPrice("AAPL")
+data = alpah_vantage2.GetListingDelistingStatus_Original()
 print(data)
